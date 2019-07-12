d2a08b3c68eb50a1c23eed73262970d6b36dc476
sam990 <sam861g@gmail.com>
2019-07-12 12:35:35 -0700

    qrcode

---
+++ controllers/test.js
@@ -0,0 +1,11 @@
+var res = require("./qr_controller");
+
+console.log(res);
+
+// // console.log(result);
+
+// // var result = res.qrProduct;
+
+// // console.log(result);
+
+// console.log(result);
