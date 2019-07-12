95b4e828fc6c13790f1368722aa00dffdcfe5bb4
Mo <Gabalyma@gmail.com>
2019-07-12 14:35:57 -0700

    drone mission added

---
+++ controllers/dronemission.js
@@ -0,0 +1,37 @@
+const { spawn } = require("child_process");
+const sdk = require("tellojs");
+
+const bindVideo = async () => {
+  const h264encoder_spawn = {
+    command: "mplayer",
+    args: ["-gui", "-nolirc", "-fps", "35", "-really-quiet", "-"]
+  };
+  const h264encoder = spawn(h264encoder_spawn.command, h264encoder_spawn.args);
+  const videoEmitter = await sdk.receiver.video.bind();
+  console.log(videoEmitter);
+  videoEmitter.on("message", msg => h264encoder.stdin.write(msg));
+};
+
+sdk.control
+  .connect()
+  .then(() => bindVideo())
+  .then(() => sdk.control.takeOff())
+  .then(() => sdk.control.move.front(20))
+  .then(() => sdk.control.move.front(20))
+  .then(() => sdk.control.move.right(76))
+  .then(() => sdk.control.move.up(140))
+  .then(() => sdk.control.move.back(20))
+  .then(() => sdk.control.move.front(70))
+  .then(() => sdk.control.move.right(76))
+  .then(() => sdk.control.move.back(20))
+  .then(() => sdk.control.move.front(20))
+  .then(() => sdk.control.move.back(70))
+  .then(() => sdk.control.flip.left())
+  .then(() => sdk.control.flip.left())
+  .then(() => sdk.control.flip.right())
+  .then(() => sdk.control.flip.right())
+  .then(() => sdk.control.flip.back())
+  .then(() => sdk.control.rotate.clockwise(360))
+  .then(() => sdk.control.land())
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
